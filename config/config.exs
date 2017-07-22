a0ba5f958d73b3934372cba677c7e48ec324aaa4
Justin Schneck <jschneck@mac.com>
2017-07-22 12:11:00 -0700

    Updated docs to reflect allowing defaults with config processor

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,3 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
-
-config :system_registry, SystemRegistry.Processor.Config,
-  priorities: [
-    :pa,
-    :pb,
-    :pc
-  ]
