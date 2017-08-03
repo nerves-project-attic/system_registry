f990664a4193b428aaf42d447e26255f5158aab4
Justin Schneck <jschneck@mac.com>
2017-08-03 11:42:33 -0400

    add simple term storage

--- test/processor/config_test.exs
+++ test/processor/config_test.exs
@@ -49,6 +49,27 @@ defmodule SystemRegistry.Processor.ConfigTest do
     assert %{config: %{^root => %{a: 3, b: 4}}} = SR.match(%{config: %{root => %{}}})
   end
 
+  test "config values can be persisted", %{root: root} do
+    scope = [:config, root, :a]
+    {_, task} = update_task(scope, 1, priority: :pa)
+    assert %{config: %{^root => %{a: 1}}} = SR.match(%{config: %{root => %{}}})
+    Process.exit(task, :kill)
+    assert %{config: %{}} = SR.match(%{config: %{root => %{}}})
+    assert :ok == SR.TermStorage.persist(scope)
+    {_, task} = update_task(scope, 1, priority: :pa)
+    assert %{config: %{^root => %{a: 1}}} = SR.match(%{config: %{root => %{}}})
+    Process.exit(task, :kill)
+    assert %{config: %{^root => %{a: 1}}} = SR.match(%{config: %{root => %{}}})
+  end
+
+  test "scopes declated in config are persisted", %{root: _root} do
+    scope = [:config, :a]
+    {_, task} = update_task(scope, 1, priority: :pa)
+    assert %{config: %{a: 1}} = SR.match(%{config: %{a: :_}})
+    Process.exit(task, :kill)
+    assert %{config: %{a: 1}} = SR.match(%{config: %{a: :_}})
+  end
+
   defp update_task(key, scope, value) do
     parent = self()
     {:ok, task} =
