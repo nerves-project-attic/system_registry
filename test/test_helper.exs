63f5f530a54f53999fab3637c68b7683c6a51fef
Justin Schneck <jschneck@mac.com>
2019-03-15 09:24:13 -0400

    Increase wait for registration test messages

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,6 +4,6 @@ Logger.remove_backend(:console)
 
 Code.compiler_options(ignore_module_conflict: true)
 
-assert_timeout = String.to_integer(System.get_env("ELIXIR_ASSERT_TIMEOUT") || "200")
+assert_timeout = String.to_integer(System.get_env("ELIXIR_ASSERT_TIMEOUT") || "500")
 
 ExUnit.start(assert_receive_timeout: assert_timeout, max_cases: 1)
